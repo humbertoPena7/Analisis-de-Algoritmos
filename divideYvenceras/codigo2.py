91d6ddc5bd8cd7c23e69fbbd603487824d2e7865
Humberto.pd <humbertodejesus.penaduenas@gmail.com>
2025-10-01 07:40:55 -0600

    Create codigo2.py

---
+++ divideYvenceras/codigo2.py
@@ -0,0 +1,16 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr  # Caso base: listas de 0 o 1 elementos ya están ordenadas
+
+    pivot = arr[len(arr) // 2]  # Selecciona el pivote (aquí usamos el elemento central)
+
+    left = [x for x in arr if x < pivot]     # Elementos menores al pivote
+    middle = [x for x in arr if x == pivot]  # Elementos iguales al pivote
+    right = [x for x in arr if x > pivot]    # Elementos mayores al pivote
+
+    return quick_sort(left) + middle + quick_sort(right)  # Llamadas recursivas
+
+# Ejemplo de uso:
+arr = [7, 38, 27, 43, 3, 9, 82, 10]
+sorted_arr = quick_sort(arr)
+print("Arreglo ordenado:", sorted_arr)
